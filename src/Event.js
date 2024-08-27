382e7d8d9e355c7bddde0ddc83b1b3e0768b0d85
Raghavendan <raghavendan2528@gmail.com>
2024-08-27 23:37:31 +0530

    css color & event page

--- src/Event.js
+++ src/Event.js
@@ -1,9 +1,187 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "./styles/Event.css";
 
-function Event(){
+const Event = () => {
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [eventName, setEventName] = useState("");
+    const [events, setEvents] = useState([]);
+    const [photo, setPhoto] = useState(null);
+    const [location, setLocation] = useState("");
     const navigate = useNavigate();
-    return(
-        <h2>Event</h2>
+
+    const Date_Click_Fun = (date) => {
+        setSelectedDate(date);
+    };
+
+    const Event_Data_Update = (event) => {
+        setEventName(event.target.value);
+    };
+
+    const Photo_Upload_Fun = (event) => {
+        setPhoto(event.target.files[0]);
+    };
+
+    const Location_Data_Update = (event) => {
+        setLocation(event.target.value);
+    };
+
+    const Create_Event_Fun = () => {
+        if (selectedDate && eventName) {
+            const newEvent = {
+                id: new Date().getTime(),
+                date: selectedDate,
+                title: eventName,
+                photo: URL.createObjectURL(photo),  // Store the photo as a URL
+                location: location,
+            };
+            setEvents([...events, newEvent]);
+            setSelectedDate(null);
+            setEventName("");
+            setPhoto(null);
+            setLocation("");
+            setSelectedDate(newEvent.date);
+        }
+    };
+
+    const Update_Event_Fun = (eventId, newName) => {
+        const updated_Events = events.map((event) => {
+            if (event.id === eventId) {
+                return {
+                    ...event,
+                    title: newName,
+                };
+            }
+            return event;
+        });
+        setEvents(updated_Events);
+    };
+
+    const Delete_Event_Fun = (eventId) => {
+        const updated_Events = events.filter((event) => event.id !== eventId);
+        setEvents(updated_Events);
+    };
+
+    return (
+        <div className="app">
+            <span className="event_t">Event</span>
+            <div className="container">
+                <div className="calendar-container">
+                    <Calendar
+                        value={selectedDate}
+                        onClickDay={Date_Click_Fun}
+                        tileClassName={({ date }) =>
+                            selectedDate &&
+                            date.toDateString() === selectedDate.toDateString()
+                                ? "selected"
+                                : events.some(
+                                      (event) =>
+                                          event.date.toDateString() ===
+                                          date.toDateString(),
+                                  )
+                                ? "event-marked"
+                                : ""
+                        }
+                    />
+                </div>
+                <div className="event-container">
+                    {selectedDate && (
+                        <div className="event-form">
+                            <h2 id="sh">Create Event</h2>
+                            <p className="sh_txt">Selected Date: {selectedDate.toDateString()}</p>
+                            <input
+                                type="text"
+                                placeholder="Event Name"
+                                value={eventName}
+                                onChange={Event_Data_Update}
+                            />
+                            <input
+                                type="file"
+                                onChange={Photo_Upload_Fun}
+                            />
+                            <input
+                                type="text"
+                                placeholder="Location"
+                                value={location}
+                                onChange={Location_Data_Update}
+                            />
+                            <button
+                                className="create-btn"
+                                onClick={Create_Event_Fun}
+                            >
+                                Click Here to Add Event
+                            </button>
+                        </div>
+                    )}
+                    {events.length > 0 && selectedDate && (
+                        <div className="event-list">
+                            <h2 id="sh">My Created Event List</h2>
+                            <div className="event-cards">
+                                {events.map((event) =>
+                                    event.date.toDateString() ===
+                                    selectedDate.toDateString() ? (
+                                        <div
+                                            key={event.id}
+                                            className="event-card"
+                                        >
+                                            
+                                            <div className="event-card-body">
+                                                
+                                                {event.photo && (
+                                                    <img
+                                                        src={event.photo}
+                                                        alt={event.title}
+                                                        className="event-photo"
+                                                    />
+                                                )}
+                                                <div className="event-card-header">
+                                                    <span className="event-date">
+                                                    🗓️{event.date.toDateString()}
+                                                    </span>                                               
+                                                </div>
+                                                <p className="event-location">
+                                                🌏{event.location}
+                                                </p>
+                                                <p className="event-title">
+                                                    {event.title}
+                                                </p>
+                                            </div>
+                                            <div className="event-actions">
+                                                    <button
+                                                        className="update-btn"
+                                                        onClick={() =>
+                                                            Update_Event_Fun(
+                                                                event.id,
+                                                                prompt(
+                                                                    "ENTER NEW TITLE",
+                                                                ),
+                                                            )
+                                                        }
+                                                    >
+                                                        Update Event
+                                                    </button>
+                                                    <button
+                                                        className="delete-btn"
+                                                        onClick={() =>
+                                                            Delete_Event_Fun(
+                                                                event.id,
+                                                            )
+                                                        }
+                                                    >
+                                                        Delete Event
+                                                    </button>
+                                                </div>
+                                        </div>
+                                    ) : null,
+                                )}
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
     );
-}
+};
+
 export default Event;
