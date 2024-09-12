ba5671fed28dd0b145aefb8b23847289257d2ca7
Raghavendan <raghavendan2528@gmail.com>
2024-09-13 00:14:38 +0530

    upd navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,61 +2,56 @@ import './styles/App.css';
 import { useRef } from "react";
 import { useNavigate } from 'react-router-dom';
 import ico from "./assets/icon.jpg";
-import { FaBars, FaTimes, FaUserShield, FaUserFriends, FaUserCircle } from "react-icons/fa";
+import { FaBars, FaTimes, FaUserShield, FaUserFriends } from "react-icons/fa";
 
 function Navbar() {
-	const navRef = useRef();
-	const navigate = useNavigate();
+  const navRef = useRef();
+  const navigate = useNavigate();
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle(
-			"responsive_nav"
-		);
-		
-	};
-	const handleSignupClick = () => {
-		navigate('/signup'); // Navigate to the signup page
-	  };
-	const admin = () => {
-		navigate('/adminlogin'); // Navigate to the signup page
-	};
+  // Toggle the navbar in mobile view
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
+  const handleSignupClick = () => {
+    navigate('/signup'); // Navigate to the signup page
+  };
+  
+  const admin = () => {
+    navigate('/adminlogin'); // Navigate to the admin login page
+  };
 
-	return (
-		<header>
-			<img src={ico} alt="KARPI Logo" className="logo-image" />
-			<h3 className="logoname">KARPI</h3>
-			<nav ref={navRef}>
-				<a href="/">Home</a>
-				<a href="/post">Post</a>
-				<a href="/chat">Chat</a>
-                <a href="/donate">Donate</a>
-				<a href="/abt">About</a>
-				<button
-					className="nav-btn nav-close-btn"
-					onClick={showNavbar}>
-					<FaTimes />
-				</button>
-			</nav>
-			<div className='parentbtn'>
-			<div className="icon-buttons">
-				<button className="icon-btn" >
-					<FaUserShield title="Admin" id='faicon' onClick={admin}/>
-				</button>
-				<button className="icon-btn" id='faicon' onClick={handleSignupClick}>
-					<FaUserFriends title="Volunteer" />
-				</button>
-				
-			</div>
-			</div>
-			
-			<button
-				className="nav-btn"
-				onClick={showNavbar}>
-				<FaBars />
-			</button>
-		</header>
-	);
+  return (
+    <header>
+      <div className="logo-section">
+        <img src={ico} alt="KARPI Logo" className="logo-image" />
+        <h3 className="logoname">KARPI</h3>
+      </div>
+      <nav ref={navRef}>
+        <a href="/">Home</a>
+        <a href="/post">Post</a>
+        <a href="/chat">Chat</a>
+        <a href="/donate">Donate</a>
+        <a href="/abt">About</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <div className='icon-buttons'>
+        <div className='btn-1' onClick={admin}>
+          <FaUserShield title="Admin" id='faicon' />
+          <span className='small'>Admin</span>
+        </div>
+        <div className='btn-2' onClick={handleSignupClick}>
+          <FaUserFriends title="Volunteer" id='faicon' />
+          <span className='small'>Volunteer</span>
+        </div>
+      </div>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
 }
 
 export default Navbar;
